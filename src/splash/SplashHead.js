9c72fbe24478decfe26e8f53db75e756530872d1
Jay Hwang <jayhwang0121@gmail.com>
2017-09-19 16:52:08 -0700

    added some styling to splash-head

--- src/splash/SplashHead.js
+++ src/splash/SplashHead.js
@@ -4,8 +4,8 @@ export default class SplashHead extends React.Component {
   render() {
     return (
       <div id="SplashHead" className='flex--center flex-direction--column'>
-        <h3>#Rethink38</h3>
-        <p>
+        <h3 className='color--white'>#Rethink38</h3>
+        <p className='color--white'>
           Rethink how you will spend $38 a month to help to support  a child in the Inner City today!
         </p>
         <button className='btn sponsor-btn' title="Sponsor a Child">
