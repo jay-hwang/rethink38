aba42cb6c74d4b584712f458f0713eef307f2f90
Jay Hwang <jayhwang0121@gmail.com>
2017-09-21 16:35:02 -0700

    refactor navbar and add logo

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './Navbar.css';
 
 class Navbar extends React.Component {
+
   render() {
     return (
       <nav id='Navbar'>
-        <span id='Navbar-link'>ABOUT US</span>
-        <span id='Navbar-link'>OUR WORK</span>
-        <span id='Navbar-link'>GET INVOLVED</span>
-        <span id='Navbar-link'>DONATE</span>
+        <div id='nav-logo'></div>
+
+        <ul id='nav-links'>
+          <li id='Navbar-link'>ABOUT US</li>
+          <li id='Navbar-link'>OUR WORK</li>
+          <li id='Navbar-link'>GET INVOLVED</li>
+          <li id='Navbar-link'>DONATE</li>
+        </ul>
       </nav>
     );
   }
--- src/splash/SplashHead.js
+++ src/splash/SplashHead.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default class SplashHead extends React.Component {
   render() {
     return (
-      <div id="SplashHead" className='flex--center flex-direction--column'>
-        <h3 className='color--white'>#Rethink38</h3>
-        <p className='color--white'>
+      <div id="SplashHead">
+        <h3 id='Rethink38-title' className='color--white'>#Rethink38</h3>
+        <p id='splashHead-p' className='color--white font-size--x'>
           Rethink how you will spend $38 a month to help to support  a child in the Inner City today!
         </p>
         <button className='btn sponsor-btn' title="Sponsor a Child">
